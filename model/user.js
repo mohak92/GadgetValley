338d9fb1517edc0e493898cda1e0a0eadca9746f
mohak92 <tamhane.mohak@gmail.com>
2019-12-16 15:13:24 -0500

    model for cart order and products

---
+++ model/user.js
@@ -0,0 +1,13 @@
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
+
+var user = {
+  all: function(cb) {
+    orm.all("users", function(res) {
+      cb(res);
+    });
+  },
+};
+
+// Export the database functions for the controller (adminController.js).
+module.exports = user;
