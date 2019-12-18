c0ed20215dfc6f6b2eb5bd612aa55e82e4209b7d
mohak92 <tamhane.mohak@gmail.com>
2019-12-18 10:51:54 -0500

    schema update

--- db/schema.sql
+++ db/schema.sql
@@ -1,33 +1,61 @@
-CREATE DATABASE gadget_valley_db;
+CREATE DATABASE  IF NOT EXISTS gadget_valley_db;
 
 USE gadget_valley_db;
 
-SHOW TABLES;
+CREATE TABLE `user_address` (
+  `id` int(11) PRIMARY KEY AUTO_INCREMENT  NOT NULL ,
+  `uid` int(11) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `address1` varchar(255) NOT NULL,
+  `address2` varchar(255) ,
+  `city` varchar(255) NOT NULL,
+  `state` varchar(255) NOT NULL,
+  `country` varchar(255) NOT NULL,
+  `zipcode` varchar(255) NOT NULL,
+   FOREIGN KEY (uid) REFERENCES users(id)
+) ;
 
-CREATE TABLE `gadgets` (
-  `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `gadgetname` varchar(255) DEFAULT NULL,
-  `image` varchar(255) DEFAULT NULL,
-  `description` text,
-  `price` double(11,2) NOT NULL DEFAULT '0.00',
-  `status` int(1) NOT NULL DEFAULT '1'
+CREATE TABLE `products` (
+  `id` int(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `stock` varchar(255) NOT NULL DEFAULT '0',
+  `price` double(11,2) NOT NULL DEFAULT '0.00'
 );
 
-CREATE TABLE `orders` (
-  `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `order_number` varchar(255) NOT NULL,
-  `gadgetname` varchar(255) NOT NULL,
-  `tax` double(10,2) DEFAULT NULL,
-  `total_amount` double(10,2) DEFAULT NULL,
-  `user_id` int(11) NOT NULL,
-  `paid` varchar(255) DEFAULT NULL,
-  `status` varchar(255) DEFAULT NULL
+CREATE TABLE `product_categories` (
+  `id` int(11) PRIMARY KEY AUTO_INCREMENT NOT NULL ,
+  `cid` int(11) NOT NULL,
+  `pid` int(11) NOT NULL
+);
+
+CREATE TABLE `categories` (
+  `id` int(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL
 ) ;
 
-CREATE TABLE `user_cart` (
-  `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) DEFAULT NULL,
-  `gadgetid` int(11) DEFAULT NULL,
-  `row_price` double(10,2) NOT NULL,
-  `tax` double(10,2) NOT NULL DEFAULT '0.00'
-) ;
+CREATE TABLE `orders` (
+  `id` int(11) PRIMARY KEY AUTO_INCREMENT NOT NULL ,
+  `uid` int(11) NOT NULL,
+  `add_id` int(11) NOT NULL,
+  `amount` varchar(255) NOT NULL,
+  `paymentmethod` varchar(255) NOT NULL
+);
+
+CREATE TABLE `order_items` (
+  `id` int(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  `oid` int(11) NOT NULL,
+  `pid` int(11) NOT NULL,
+  `product_price` varchar(255) NOT NULL,
+  `product_quantity` varchar(255) NOT NULL
+  );
+
+CREATE TABLE `order_status` (
+  `id` int(11) PRIMARY KEY AUTO_INCREMENT NOT NULL ,
+  `oid` int(11) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `notes` varchar(255) NOT NULL
+);
