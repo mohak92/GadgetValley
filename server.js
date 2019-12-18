2391bb63c26cdacddd7059abd1f6ded1f226e845
mohak92 <tamhane.mohak@gmail.com>
2019-12-18 10:50:53 -0500

    landing page route fixes

--- server.js
+++ server.js
@@ -25,12 +25,6 @@ app.set('view engine', '.handlebars');
 
 app.use(express.static("public"));
 
-app.get('/', function (req, res) {
-
-    res.render('index');
-
-});
-
 //Models
 var models = require("./models");
 
