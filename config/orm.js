ba8bd2f107bd12e7feb7b72b89fc4fd00f23ab15
mohak92 <tamhane.mohak@gmail.com>
2019-12-18 10:53:03 -0500

    queries modified

--- config/orm.js
+++ config/orm.js
@@ -70,7 +70,7 @@ var orm = {
       cb(result);
     });
   },
-  // An example of objColVals would be {name: panther, sleepy: true}
+  // An example of objColVals would be {firstName: John, lastName: Doe}
   update: function(table, objColVals, condition, cb) {
     var queryString = "UPDATE " + table;
 
@@ -90,5 +90,5 @@ var orm = {
   }
 };
 
-// Export the orm object for the model (cat.js).
+// Export the orm object for the models
 module.exports = orm;
