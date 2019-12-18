acffa2cf307ba999d2e2854352952ee9a304b692
mohak92 <tamhane.mohak@gmail.com>
2019-12-18 15:39:25 -0500

    seed data

---
+++ db/seeds.sql
@@ -0,0 +1,31 @@
+INSERT INTO `gadget_valley_db`.`products` (`title`, `description`, `image`, `type`, `stock`, `price`) 
+VALUES ('iPhone 11 Pro Max', 'iPhone 11 Pro Max 64Gb, 256GB, 512GB A13 Bionic Processor, FaceID', '
+https://images.macrumors.com/article-new/2019/09/iphone-11-and-11-pro.jpg', 'SmartPhone', '29047899', '69.99'),
+('iPhone 11 Pro', 'iPhone 11 Pro 64Gb, 256GB, 512GB A13 Bionic Processor, FaceID', '
+https://images.macrumors.com/article-new/2019/09/iphone-11-and-11-pro.jpg', 'SmartPhone', '29047899', '59.99'),
+('Apple Watch Series 5', 'Always on Display EKG, Health and Fitness', '
+https://venturebeat.com/wp-content/uploads/2019/09/applewatchos6-e1568129368598.jpg?fit=400%2C225&strip=all', 'SmartWatch', '290547829', '39.99'),
+('Apple Watch Series 4', 'EKG, Health and Fitness', '
+https://venturebeat.com/wp-content/uploads/2019/09/applewatchos6-e1568129368598.jpg?fit=400%2C225&strip=all', 'SmartWatch', '290547829', '39.99'),
+('Google Pixel 4', 'Android smartphone from google 64GB and 128 GB', '
+https://www.irishtimes.com/polopoly_fs/1.4065990.1572365450!/image/image.jpg_gen/derivatives/landscape_620/image.jpg', 'SmartPhone', '29047899', '33.99'),
+('Google Pixel 4 XL', 'Android smartphone from google 64GB and 128 GB and bigger screen', '
+https://www.irishtimes.com/polopoly_fs/1.4065990.1572365450!/image/image.jpg_gen/derivatives/landscape_620/image.jpg', 'SmartPhone', '29047899', '43.99'),
+('Galaxy Watch Active 2', 'The Galaxy Watch Active 2 looks and feels a lot like its predecessor -- both have 
+a circular AMOLED touchscreen -- but the Active 2 now comes in two sizes (40mm or 44mm) and adds an LTE option', '
+https://image-us.samsung.com/SamsungUS/home/mobile/wearables/pdp/sm-r820nzkaxar/Gallery-Active2-BT-44mm-AquaBlack-1.jpg?$product-details-jpg$', 'SmartWatch', '29047899', '12.99'),
+('Samsung galaxy s10', 'The phone comes with a 6.10-inch touchscreen display and an aspect ratio of 19:9. 
+Samsung Galaxy S10 is powered by a 1.9GHz octa-core Samsung Exynos 9820 SoC processor', '
+https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/galaxy-s10-onleaks-cropped-1550679095.jpg?crop=0.752xw:1.00xh;0.125xw,0&resize=640:*', 'SmartPhones', '29047899', '59.99'),
+('Oneplus 7 pro', 'OnePlus 7 Pro, Operating System: OxygenOS based on Android 9 (Pie), CPU: 
+Qualcomm Snapdragon 855 (Octa-core, 7nm processor), 8GB Memory + 256GB Storage, In-Display Fingerprint Sensor, Dual SIM ', '
+https://images-na.ssl-images-amazon.com/images/I/61W2CUo1MPL._SX569_.jpg', 'SmartPhone', '29047899', '24.99'),
+('Nintendo Switch', 'Nintendo Switch, the new home video game system from Nintendo. In addition to providing single and multiplayer thrills at home', '
+https://target.scene7.com/is/image/Target/GUEST_5561e25a-a986-4b57-bba4-ee339796ae89?wid=488&hei=488&fmt=pjpeg', 'GamingConsole', '29047899', '15.99'),
+('PS4', 'The PlayStation 4 (officially abbreviated as PS4) is an eighth-generation home video game console developed by Sony Interactive Entertainment', '
+https://www.adorama.com/images/Large/so3003346.jpg', 'GamingConsole', '289047899', '39.99'),
+('Xbox One X', 'Xbox One X is the  most powerful console, with 40% more power than any other console 4K Gaming','
+https://assets.pcmag.com/media/images/471937-microsoft-xbox-one-x.jpg?width=333&height=245', 'GamingConsole', '290477899', '49.99');
+
+SELECT * FROM Products;
+
